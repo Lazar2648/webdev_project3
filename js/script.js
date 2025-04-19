7616b5a064e7e152f35ad743f40de9b7cdfb4701
Lazar2648 <alazarowicz@hawk.iit.edu>
2025-04-19 01:13:32 -0500

    started js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-function init(){
-	
+function initMap() {
+  var el = document.getElementById("canvas");
+  var myLocation = new google.maps.LatLng(46.429929664728206, 6.911790784724885);
+  var mapOptions = {
+  	center: myLocation,
+  	zoom: 14,
+  	mapTypeId: google.maps.MapTypeId.SATELLITE,
+  	mapTypeControlOptions: {
+  		position: google.maps.ControlPosition.BOTTOM_CENTER
+  	} 
+  };
+
+
